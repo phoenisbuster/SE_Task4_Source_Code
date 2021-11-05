2ea7a5cba3af5b7c341ba44f0d9cbee9221f57d1
phoenisbuster <phuc.nguyenphoenis@hcmut.edu.vn>
2021-11-05 22:00:26 +0700

    Add footer, Update title web app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import React, { useState } from 'react';
 import Navbar from './Components/Navigation Bar/Navbar';
 import GenerateMenu from './Components/Menu/GenerateMenu';
 import GenerateCart from './Components/Cart/GenerateCart';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 	return (
 		<main>
 			<Navbar />
 			<GenerateMenu />
-			<GenerateCart /> 		
+			<GenerateCart /> 	
+			<Footer />	
 		</main>
+		
 	)
 };
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,146 @@
+import {
+    Facebook,
+    Instagram,
+    MailOutline,
+    Phone,
+    Pinterest,
+    Room,
+    Twitter,
+  } from "@material-ui/icons";
+  import styled from "styled-components";
+  import React, { Component }  from 'react';
+  import './Footer.css';
+
+  const Container = styled.div`
+    display: flex;
+  `;
+  
+  const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  `;
+  
+  const Logo = styled.h1``;
+  
+  const Desc = styled.p`
+    margin: 20px 0px;
+  `;
+  
+  const SocialContainer = styled.div`
+    display: flex;
+  `;
+  
+  const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${(props) => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+  `;
+  
+  const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+  `;
+  
+  const Title = styled.h3`
+    margin-bottom: 30px;
+  `;
+  
+  const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+  `;
+  
+  const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+  `;
+  
+  const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+  `;
+  
+  const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+  `;
+  
+  const Payment = styled.img`
+      width: 50%;
+  `;
+  
+  const Footer = () => {
+    return (
+      <Container>
+        <Left>
+          <Logo>Members</Logo>
+          <Desc>
+            Nguyen Dinh Bao Phuc -- 1852068
+          </Desc>
+          <Desc>
+            Hoang Nhat Quang -- 1852???
+          </Desc>
+          <Desc>
+            Nguyen Minh Anh -- 1852???
+          </Desc>
+          <Desc>
+            Tran Anh Dung -- 1852???
+          </Desc>
+          <Desc>
+            Dang Tran Dat -- 1852???
+          </Desc>
+          <SocialContainer>
+            <SocialIcon color="3B5999">
+              <Facebook />
+            </SocialIcon>
+            <SocialIcon color="E4405F">
+              <Instagram />
+            </SocialIcon>
+            <SocialIcon color="55ACEE">
+              <Twitter />
+            </SocialIcon>
+            <SocialIcon color="E60023">
+              <Pinterest />
+            </SocialIcon>
+          </SocialContainer>
+        </Left>
+        <Center>
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Menu</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+        </Center>
+        <Right>
+          <Title>Contact</Title>
+          <ContactItem>
+            <Room style={{marginRight:"10px"}}/> 268 Lý Thường Kiệt, Phường 14, Quận 10, Thành phố Hồ Chí Minh
+          </ContactItem>
+          <ContactItem>
+            <Phone style={{marginRight:"10px"}}/> +84 69696969
+          </ContactItem>
+          <ContactItem>
+            <MailOutline style={{marginRight:"10px"}} /> phuc.nguyenphoenis@hcmut.edu.vn
+          </ContactItem>
+          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+        </Right>
+      </Container>
+    );
+  };
+  
+  export default Footer;
